06e29fba6e2e87b05a278b8e5a0eefe51314b8d1
Urja Ramanandi <urja.ramanandi@vimcar.com>
2020-09-17 13:48:43 +0200

    Working S3 bucket creation, permission is not working

--- bin/maven-s3.ts
+++ bin/maven-s3.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { MavenS3Stack } from '../lib/maven-s3-stack';
 
 const app = new cdk.App();
-new MavenS3Stack(app, 'MavenS3Stack');
+new MavenS3Stack(app, 'MavenS3Stack',{
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION 
+    }
+});
--- lib/maven-s3-stack.ts
+++ lib/maven-s3-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as iam from '@aws-cdk/aws-iam';
+import { BlockPublicAccess } from '@aws-cdk/aws-s3';
+import { Group } from '@aws-cdk/aws-iam';
 
 export class MavenS3Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const fortranDeveloper = "arn:aws:iam::898915602003:group/fortran-developers";
+    const fortranDeveloper2 = "arn:aws:iam::898915602003:group/fortran-developers-2";
+    const deploymentGroup ="arn:aws:iam::898915602003:group/deployment";
 
-    // The code that defines your stack goes here
+    // S3
+    const bucket = new s3.Bucket(this, "MavenBucketTest", {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL
+    });
+    
+    bucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions:['s3:GetObject','s3:PutObject'],
+      resources: [bucket.bucketArn +"/*"],
+      principals: [new iam.AccountPrincipal(fortranDeveloper)]
+    }));
   }
 }
